917dc5d62527da79a6194782aba46a04cfcc2e8f
lost-incode <vicktoriaap@gmail.com>
2021-08-01 18:43:25 +0300

    Add trip filters to index.html via js module

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {createSiteMenuTemplate} from './view/menu.js';
 import {createSiteTripInfoTemplate} from './view/trip-info.js';
 import {createSiteTripCostTemplate} from './view/trip-cost.js';
+import {createSiteTripFiltersTemplate} from './view/filters.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -9,8 +10,10 @@ const render = (container, template, place) => {
 const siteHeaderElement = document.querySelector('.page-header');
 const siteNavigationElement = siteHeaderElement.querySelector('.trip-controls__navigation');
 const siteTripMainElement = siteHeaderElement.querySelector('.trip-main');
+const siteTripFiltersElement = siteTripMainElement.querySelector('.trip-controls__filters');
 
 render(siteNavigationElement, createSiteMenuTemplate(), 'beforeend');
 render(siteTripMainElement, createSiteTripInfoTemplate(), 'afterbegin');
 const siteTripInfoSection = siteTripMainElement.querySelector('.trip-main__trip-info');
 render(siteTripInfoSection, createSiteTripCostTemplate(), 'beforeend');
+render(siteTripFiltersElement, createSiteTripFiltersTemplate(), 'beforeend');
