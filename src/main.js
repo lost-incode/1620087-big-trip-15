e3c97df4805a0726431c96be1010b741af3f5321
lost-incode <vicktoriaap@gmail.com>
2021-08-01 18:25:13 +0300

    Add trip cost to index.html via js module

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import {createSiteMenuTemplate} from './view/menu.js';
 import {createSiteTripInfoTemplate} from './view/trip-info.js';
+import {createSiteTripCostTemplate} from './view/trip-cost.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -11,3 +12,5 @@ const siteTripMainElement = siteHeaderElement.querySelector('.trip-main');
 
 render(siteNavigationElement, createSiteMenuTemplate(), 'beforeend');
 render(siteTripMainElement, createSiteTripInfoTemplate(), 'afterbegin');
+const siteTripInfoSection = siteTripMainElement.querySelector('.trip-main__trip-info');
+render(siteTripInfoSection, createSiteTripCostTemplate(), 'beforeend');
