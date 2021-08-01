cf14b061745bde0f3bd29a6a63e4fd4bb5037770
lost-incode <vicktoriaap@gmail.com>
2021-08-01 18:57:26 +0300

    Add trip sorting to index.html via js module

--- src/main.js
+++ src/main.js
@@ -2,18 +2,23 @@ import {createSiteMenuTemplate} from './view/menu.js';
 import {createSiteTripInfoTemplate} from './view/trip-info.js';
 import {createSiteTripCostTemplate} from './view/trip-cost.js';
 import {createSiteTripFiltersTemplate} from './view/filters.js';
+import {createSiteTripSortingTemplate} from './view/sorting.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const siteHeaderElement = document.querySelector('.page-header');
+const siteMainElement = document.querySelector('.page-main');
 const siteNavigationElement = siteHeaderElement.querySelector('.trip-controls__navigation');
 const siteTripMainElement = siteHeaderElement.querySelector('.trip-main');
 const siteTripFiltersElement = siteTripMainElement.querySelector('.trip-controls__filters');
+const siteTripEventsElement = siteMainElement.querySelector('.trip-events');
+
 
 render(siteNavigationElement, createSiteMenuTemplate(), 'beforeend');
 render(siteTripMainElement, createSiteTripInfoTemplate(), 'afterbegin');
 const siteTripInfoSection = siteTripMainElement.querySelector('.trip-main__trip-info');
 render(siteTripInfoSection, createSiteTripCostTemplate(), 'beforeend');
 render(siteTripFiltersElement, createSiteTripFiltersTemplate(), 'beforeend');
+render(siteTripEventsElement, createSiteTripSortingTemplate(), 'beforeend');
