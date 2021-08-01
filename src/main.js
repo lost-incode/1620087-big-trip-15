6cb539bda4c942756d8365e2625d765f697639f8
lost-incode <vicktoriaap@gmail.com>
2021-08-01 19:15:28 +0300

    Add form for addition new event to index.html via js module

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import {createSiteTripInfoTemplate} from './view/trip-info.js';
 import {createSiteTripCostTemplate} from './view/trip-cost.js';
 import {createSiteTripFiltersTemplate} from './view/filters.js';
 import {createSiteTripSortingTemplate} from './view/sorting.js';
+import {createSiteTripEventsListTemplate} from './view/events-list.js';
+import {createSiteAddFormTemplate} from './view/add-form.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -18,7 +20,15 @@ const siteTripEventsElement = siteMainElement.querySelector('.trip-events');
 
 render(siteNavigationElement, createSiteMenuTemplate(), 'beforeend');
 render(siteTripMainElement, createSiteTripInfoTemplate(), 'afterbegin');
+
 const siteTripInfoSection = siteTripMainElement.querySelector('.trip-main__trip-info');
+
 render(siteTripInfoSection, createSiteTripCostTemplate(), 'beforeend');
 render(siteTripFiltersElement, createSiteTripFiltersTemplate(), 'beforeend');
 render(siteTripEventsElement, createSiteTripSortingTemplate(), 'beforeend');
+render(siteTripEventsElement, createSiteTripEventsListTemplate(), 'beforeend');
+
+const siteTripEventsListElement = siteTripEventsElement.querySelector('.trip-events__list');
+render(siteTripEventsListElement, createSiteAddFormTemplate(), 'beforeend');
+
+
