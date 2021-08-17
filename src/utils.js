637c1a062af3fcd93dd0271b5ca179f4a25f2a44
lost-incode <vicktoriaap@gmail.com>
2021-08-17 22:05:51 +0300

    Change switch to dictionary

--- src/utils.js
+++ src/utils.js
@@ -3,15 +3,13 @@ export const RenderPosition = {
   BEFOREEND: 'beforeend',
 };
 
+const RenderMethod = {
+  [RenderPosition.AFTERBEGIN]: 'prepend',
+  [RenderPosition.BEFOREEND]: 'append',
+};
+
 export const render = (container, element, place = RenderPosition.BEFOREEND) => {
-  switch (place) {
-    case RenderPosition.AFTERBEGIN:
-      container.prepend(element);
-      break;
-    case RenderPosition.BEFOREEND:
-      container.append(element);
-      break;
-  }
+  container[RenderMethod[place]](element);
 };
 
 export const createElement = (template) => {
@@ -24,5 +22,6 @@ export const createElement = (template) => {
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
+
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
