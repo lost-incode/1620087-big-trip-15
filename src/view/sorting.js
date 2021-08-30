78156c9c8b21b898577fbe7b8cba5079dbf60c06
lost-incode <vicktoriaap@gmail.com>
2021-08-30 13:18:12 +0300

    Delete comments

--- src/view/sorting.js
+++ src/view/sorting.js
@@ -46,7 +46,6 @@ export default class TripSorting extends AbstractView {
       return;
     }
 
-    // evt.preventDefault();
     this._callback.sortTypeChange(evt.target.dataset.sortType);
   }
 
