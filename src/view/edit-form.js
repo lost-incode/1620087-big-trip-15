c2613598b832ceced408637f02b1ce36fdf77e93
lost-incode <vicktoriaap@gmail.com>
2021-09-10 12:46:06 +0300

    Add deleting functional for points

--- src/view/edit-form.js
+++ src/view/edit-form.js
@@ -118,6 +118,7 @@ const createSiteEditFormTemplate = ({type, startDate, endDate, point, offers, de
 export default class EditingForm extends SmartView {
   constructor(point = DEFAULT_POINT) {
     super();
+
     this._data = EditingForm.parsePointToData(point);
     this._datepickerStartDate = null;
     this._datepickerEndDate = null;
@@ -126,6 +127,7 @@ export default class EditingForm extends SmartView {
     this._formSubmitHandler = this._formSubmitHandler.bind(this);
     this._startDateChangeHandler = this._startDateChangeHandler.bind(this);
     this._endDateChangeHandler = this._endDateChangeHandler.bind(this);
+    this._formDeleteClickHandler = this._formDeleteClickHandler.bind(this);
     this._typeChangeHandler = this._typeChangeHandler.bind(this);
     this._cityChangeHandler = this._cityChangeHandler.bind(this);
     this._priceChangeHandler = this._priceChangeHandler.bind(this);
@@ -133,6 +135,22 @@ export default class EditingForm extends SmartView {
 
     this._setInnerHandlers();
     this._setDatepickers();
+  }
+
+  // Перегружаем метод родителя removeElement,
+  // чтобы при удалении удалялся более ненужный календарь
+  removeElement() {
+    super.removeElement();
+
+    if (this._datepickerStartDate) {
+      this._datepickerStartDate.destroy();
+      this._datepickerStartDate = null;
+    }
+
+    if (this._datepickerEndDate) {
+      this._datepickerEndDate.destroy();
+      this._datepickerEndDate = null;
+    }
   }
 
   reset(point) {
@@ -150,6 +168,7 @@ export default class EditingForm extends SmartView {
     this._setDatepickers();
     this.setFormSubmitHandler(this._callback.formSubmit);
     this.setPointClickHandler(this._callback.click);
+    this.setDeleteClickHandler(this._callback.deleteClick);
   }
 
   _setDatepickers() {
@@ -269,6 +288,16 @@ export default class EditingForm extends SmartView {
   setFormSubmitHandler(callback) {
     this._callback.formSubmit = callback;
     this.getElement().querySelector('form').addEventListener('submit', this._formSubmitHandler);
+  }
+
+  _formDeleteClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.deleteClick(EditingForm.parseDataToPoint(this._data));
+  }
+
+  setDeleteClickHandler(callback) {
+    this._callback.deleteClick = callback;
+    this.getElement().querySelector('.event__reset-btn').addEventListener('click', this._formDeleteClickHandler);
   }
 
   static parsePointToData(point) {
